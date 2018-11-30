d8b6da75e5ff3589ac5562f2b54945d898e74d0b
Matt Bledsoe <mattbledsoe@Matts-iMac.local>
2018-11-30 09:13:10 -0500

    final

--- index.js
+++ index.js
@@ -1,92 +1,91 @@
-
 // Hamburger Logic ///////////////////////////////////
 
-var menu = document.getElementById("hamburger"),
-    x = document.getElementById("nav");
+  var menu = document.getElementById("hamburger"),
+      x = document.getElementById("nav");
 
-function menuShow() {
-  if (x.style.display === "none") {
-    x.style.display = "flex";
-  } else {
-    x.style.display = "none"
+  function menuShow() {
+    if (x.style.display == " " || x.style.display == "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
   }
-}
 
 
-// Menu Logic //////////////////////////////////////////
+  // Menu Logic //////////////////////////////////////////
 
-var law = document.getElementById("law"),
-    drop = document.getElementById("dropdown");
+  var law = document.getElementById("law"),
+      drop = document.getElementById("dropdown");
 
-law.addEventListener("mouseover", function(){
-  drop.style.display = "flex";
-}, false);
+  law.addEventListener("mouseover", function(){
+    drop.style.display = "flex";
+  }, false);
 
-law.addEventListener("mouseout", function(){
-  drop.style.display = "none";
-}, false);
+  law.addEventListener("mouseout", function(){
+    drop.style.display = "none";
+  }, false);
 
-drop.addEventListener("mouseover", function(){
-  this.style.display = "flex";
-}, false);
+  drop.addEventListener("mouseover", function(){
+    this.style.display = "flex";
+  }, false);
 
-drop.addEventListener("mouseout", function(){
-  this.style.display = "none";
-}, false);
+  drop.addEventListener("mouseout", function(){
+    this.style.display = "none";
+  }, false);
 
-var about = document.getElementById("about"),
-    dropAbout = document.getElementById("dropdown-about");
+  var about = document.getElementById("about"),
+      dropAbout = document.getElementById("dropdown-about");
 
-about.addEventListener("mouseover", function(){
-  dropAbout.style.display = "flex";
-}, false);
+  about.addEventListener("mouseover", function(){
+    dropAbout.style.display = "flex";
+  }, false);
 
-about.addEventListener("mouseout", function(){
-  dropAbout.style.display = "none";
-}, false);
+  about.addEventListener("mouseout", function(){
+    dropAbout.style.display = "none";
+  }, false);
 
-dropAbout.addEventListener("mouseover", function(){
-  this.style.display = "flex";
-}, false);
+  dropAbout.addEventListener("mouseover", function(){
+    this.style.display = "flex";
+  }, false);
 
-dropAbout.addEventListener("mouseout", function(){
-  this.style.display = "none";
-}, false);
+  dropAbout.addEventListener("mouseout", function(){
+    this.style.display = "none";
+  }, false);
 
-var areas = document.getElementById("areas"),
-    dropAreas = document.getElementById("dropdown-areas");
+  var areas = document.getElementById("areas"),
+      dropAreas = document.getElementById("dropdown-areas");
 
-areas.addEventListener("mouseover", function(){
-  dropAreas.style.display = "flex";
-}, false);
+  areas.addEventListener("mouseover", function(){
+    dropAreas.style.display = "flex";
+  }, false);
 
-areas.addEventListener("mouseout", function(){
-  dropAreas.style.display = "none";
-}, false);
+  areas.addEventListener("mouseout", function(){
+    dropAreas.style.display = "none";
+  }, false);
 
-dropAreas.addEventListener("mouseover", function(){
-  this.style.display = "flex";
-}, false);
+  dropAreas.addEventListener("mouseover", function(){
+    this.style.display = "flex";
+  }, false);
 
-dropAreas.addEventListener("mouseout", function(){
-  this.style.display = "none";
-}, false);
+  dropAreas.addEventListener("mouseout", function(){
+    this.style.display = "none";
+  }, false);
 
 
-// Resources accordion logic /////////////////////////////////////////
+  // Resources accordion logic /////////////////////////////////////////
 
-var acc = document.getElementsByClassName("accordion");
-var i;
+  var acc = document.getElementsByClassName("accordion");
+  var i;
 
-for(i = 0; i < acc.length; i++) {
-	acc[i].onclick = function() {
-		this.classList.toggle("active");
-		var panel = this.nextElementSibling;
+  for(i = 0; i < acc.length; i++) {
+  	acc[i].onclick = function() {
+  		this.classList.toggle("active");
+  		var panel = this.nextElementSibling;
 
-		if(panel.style.maxHeight) {
-			panel.style.maxHeight = null;
-		}	else {
-			panel.style.maxHeight = panel.scrollHeight + "px"
-		}
-	}
-}
+  		if(panel.style.maxHeight) {
+  			panel.style.maxHeight = null;
+  		}	else {
+  			panel.style.maxHeight = panel.scrollHeight + "px"
+  		}
+  	}
+  }
